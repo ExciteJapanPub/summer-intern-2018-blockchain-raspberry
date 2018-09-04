467555b7718d304184133bb5aaea1d2706411801
佐藤 知哉 <tomoya.sato@dhcpp194.agt.excite.co.jp>
2018-09-04 15:42:37 +0900

    [delete]不要なコメント等を削除

--- sample/input/joy_stick.py
+++ sample/input/joy_stick.py
@@ -1,6 +1,5 @@
 import spidev
 import time
-import os
 
 class AnalogSensorReader:
     def __init__(self):
@@ -35,27 +34,7 @@ class AnalogSensorReader:
         return volts
 
 
-
-def convert_temp(data, places):
-    # ADC Value
-    # (approx)  Temp  Volts
-    #    0      -50    0.00
-    #   78      -25    0.25
-    #  155        0    0.50
-    #  233       25    0.75
-    #  310       50    1.00
-    #  465      100    1.50
-    #  775      200    2.50
-    # 1023      280    3.30
-
-    temp = ((data * 330) / float(1023)) - 50
-    temp = round(temp, places)
-    return temp
-
-
-
 if __name__ == "__main__":
-
     # Define sensor channels
     x_channel = 0
     y_channel = 1
@@ -76,16 +55,10 @@ if __name__ == "__main__":
             y_volts = asr.convert_volts(y_level, 2)
             sw_volts = asr.convert_volts(sw_level, 2)
 
-            # Read the temperature sensor data
-            #temp_level = asr.read_row_data_for_channel(temp_channel)
-            #temp_volts = asr.convert_volts(temp_level, 2)
-            #temp = convert_temp(temp_level, 2)
-
             print(f"-------------------- loop: {i} ------------------------")
             print(f"x: {x_level} ({x_volts}V)")
             print(f"y: {y_level} ({y_volts}V)")
             print(f"SW: {sw_level} ({sw_volts}V)")
-
 
             time.sleep(delay)
 
